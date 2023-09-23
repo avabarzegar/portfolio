51b6b99584a44ff40cf18cb024762de8b5d1d4a5
fateme-barzegar <barzegarfa81@gmail.com>
2023-09-23 10:22:14 -0400

    replace my resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/Resume.pdf'
 import profile from '../assets/jpg/mypic.jpg'
 
 export const headerData = {
-    name: 'Fateme (Ava) Barzegar',
+    name: 'Ava Barzegar',
     title: "Web Developer",
     desciption:"Simplicity is the soul of efficiency.",
     image: profile,
