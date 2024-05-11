a3681c7a7f696eed30939bcbb0d4eded54b18208
fateme-barzegar <barzegarfa81@gmail.com>
2024-05-11 19:12:31 -0400

    add files

---
+++ src/data/experienceData.js
@@ -0,0 +1,23 @@
+export const experienceData = [
+    {
+        id: 3,
+        company: 'Web Arka Co, Shiraz, Iran ',
+        jobtitle: 'Frontend Developer',
+        startYear: '08-2022',
+        endYear: '05-2023'  
+    },
+    {
+        id: 2,
+        company: 'Front-End Developer as a Freelancer ',
+        jobtitle: 'Frontend Developer',
+        startYear: '08-2021',
+        endYear: '07-2022'
+    },
+    {
+        id: 1,
+        company: 'Rabsana Co, Shiraz, Iran ',
+        jobtitle: 'Frontend Developer',
+        startYear: '02-2021',
+        endYear: '07-2021'
+    },
+]
